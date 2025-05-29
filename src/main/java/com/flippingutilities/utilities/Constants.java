f1785b84dd6845f7671c0580c30d611baa7e5dde
Antony Garand <9059487+AntonyGarand@users.noreply.github.com>
2025-05-29 08:56:01 -0400

    Update item cap tax amount

--- src/main/java/com/flippingutilities/utilities/Constants.java
+++ src/main/java/com/flippingutilities/utilities/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     //enough approximation
     public static int GE_TAX_START = 1639072800;
     public static int GE_TAX_CAP = 5000000;
-    public static int MAX_PRICE_FOR_GE_TAX = 500000000;
+    public static int MAX_PRICE_FOR_GE_TAX = 250000000;
     public static double GE_TAX = 0.02; // 2%
     public static int BOND_ID = 13190;
     public static String DUMMY_ITEM = "dummy";
