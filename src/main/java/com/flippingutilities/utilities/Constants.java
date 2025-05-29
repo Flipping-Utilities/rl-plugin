04e28148c29f2fd4e1522d414f5c760744c20c02
Antony Garand <9059487+AntonyGarand@users.noreply.github.com>
2025-05-29 08:56:01 -0400

    Update GE tax amount

--- src/main/java/com/flippingutilities/utilities/Constants.java
+++ src/main/java/com/flippingutilities/utilities/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static int GE_TAX_START = 1639072800;
     public static int GE_TAX_CAP = 5000000;
     public static int MAX_PRICE_FOR_GE_TAX = 500000000;
-    public static double GE_TAX = 0.01; // 1%
+    public static double GE_TAX = 0.02; // 2%
     public static int BOND_ID = 13190;
     public static String DUMMY_ITEM = "dummy";
 
